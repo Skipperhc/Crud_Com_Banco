86a4eb99294f9151ff1c21049cc353b6adf622fa
skipperhc <vitorhainosz2@gmail.com>
2020-01-21 15:33:11 -0300

    iniciando o curso da udemy

---
+++ Crud_Com_Banco/Data/Crud_Com_BancoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Crud_Com_Banco.Models;
+
+namespace Crud_Com_Banco.Data
+{
+    public class Crud_Com_BancoContext : DbContext
+    {
+        public Crud_Com_BancoContext (DbContextOptions<Crud_Com_BancoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Crud_Com_Banco.Models.Department> Department { get; set; }
+    }
+}
---
+++ Crud_Com_Banco/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Crud_Com_Banco.Models {
+    public class Department {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
