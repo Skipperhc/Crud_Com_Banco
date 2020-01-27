e1400314a6aac04aebd2612dfb894dd83c8c0bfc
skipperhc <vitorhainosz2@gmail.com>
2020-01-27 13:17:08 -0300

    adicionando validação

--- Crud_Com_Banco/Controllers/SellersController.cs
+++ Crud_Com_Banco/Controllers/SellersController.cs
@@ -32,6 +32,11 @@ namespace Crud_Com_Banco.Controllers {
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller) {
+            if (!ModelState.IsValid) {//!ModelState.IsValid = se p modelstate não foi validado, ele entra e quebra o método
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
 
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
@@ -87,13 +92,19 @@ namespace Crud_Com_Banco.Controllers {
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller) {
-            if (id != seller.Id) {
-                return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
+            if (!ModelState.IsValid) {//!ModelState.IsValid = se p modelstate não foi validado, ele entra e quebra o método
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }//para evitar que a pessoa sem javascript envie um cadastro vazio
+
+            if (id != seller.Id) {//confirma se o id que estamos tentando editar é o mesmo que está sendo mostrado
+                return RedirectToAction(nameof(Error), new { message = "Id mismatch" });//tratamento do erro, ids não correspondem
             }
             try {
                 _sellerService.Update(seller);
                 return RedirectToAction(nameof(Index));
-            } catch (ApplicationException e) {
+            } catch (ApplicationException e) {//ApplicationException tratamento geral de erros, se o id não existe ou não é o mesmo
                 return RedirectToAction(nameof(Error), new { message = e.Message });
             }
         }
--- Crud_Com_Banco/Models/Seller.cs
+++ Crud_Com_Banco/Models/Seller.cs
@@ -7,18 +7,33 @@ using System.Threading.Tasks;
 namespace Crud_Com_Banco.Models {
     public class Seller {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} Required")] //diz que é obrigatório ter isso preenchido, o {0} passa o nome da variavel
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} Name size should be between {2} and {1}")]
+        //mostra o máximo e o mínimo requerido da string, {0} passa o nome da variavel, {2} o minimo {1} máximo
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} Required")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} Required")]
         [Display(Name="Birth Date")] //customiza o que aparece na coluna
         [DataType(DataType.Date)] //muda de texto para um link de email mesmo
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]//para definir qual formato vai aparecer, no caso dia/mes/ano
         public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0} Required")]
         [Display(Name = "Salario")]//mudo o nome que aparece na coluna
         [DisplayFormat(DataFormatString ="{0:F2}")]//escolher quantos zeros depois da vircula mostrar
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} To {2}")]//estou falando o min e o max que o salario deve ter
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
+
         public int DepartmentId { get; set; }
+
 
         //public Boolean active { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
